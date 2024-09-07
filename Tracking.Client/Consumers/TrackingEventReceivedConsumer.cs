5e442a21f8a5ea1bef052eecd372246c1eba3e1d
blazknuplez <bknuplez@gmail.com>
2024-09-07 16:36:45 +0200

    Added client

---
+++ Tracking.Client/Consumers/TrackingEventReceivedConsumer.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Tracking.Contracts.Events;
+
+namespace Tracking.Client.Consumers;
+
+public class TrackingEventReceivedConsumer : IConsumer<TrackingEventReceived>
+{
+    private readonly ILogger<TrackingEventReceivedConsumer> _logger;
+
+    public TrackingEventReceivedConsumer(ILogger<TrackingEventReceivedConsumer> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Consume(ConsumeContext<TrackingEventReceived> context)
+    {
+        _logger.LogInformation("Received event: {AccountId} - {Data}", context.Message.AccountId, context.Message.Data);
+        return Task.CompletedTask;
+    }
+}
---
+++ Tracking.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using MassTransit;
+using Microsoft.Extensions.Hosting;
+using Tracking.Client.Consumers;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddMassTransit(x =>
+        {
+            x.AddConsumer<TrackingEventReceivedConsumer>();
+            x.UsingRabbitMq((context,cfg) =>
+            {
+                cfg.Host("localhost", "/", h => { h.Username("guest"); h.Password("guest"); });
+                cfg.ConfigureEndpoints(context);
+            });
+        });
+    });
+
+await host.RunConsoleAsync();
